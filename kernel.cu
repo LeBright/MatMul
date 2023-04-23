edc80347a3efbee95575cbfe0662c70743ab655a
LiLiang <616795022@qq.com>
2023-04-23 18:48:20 +0800

    matmul

--- kernel.cu
+++ kernel.cu
@@ -2,83 +2,236 @@
 #include <stdio.h>
 #include <Eigen/Core>
 #include <ctime>
+#include <Windows.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <cublas_v2.h>
+
 using namespace std;
 
-const unsigned int Left_Row = 453;
-const unsigned int Left_Col =222;
-const unsigned int Right_Row = 222;
-const unsigned int Right_Col = 357;
+const unsigned int Left_Row = 12800;
+const unsigned int Left_Col = 12800;
+const unsigned int Right_Row = 12800;
+const unsigned int Right_Col = 12800;
 bool CPU = false;
 bool GPU = false;
 
-typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>MatrixFDD;
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>MatrixFDD_Col;
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>MatrixFDD_Row;
 
-__device__ int my_ceil(float A) {
-	if (A > (int)A)
-		return (int)A + 1;
-	else
-		return (int)A;
-}
-__global__ void MatMul(float* left, float* right, float* answer, unsigned int left_size, unsigned int left_stride, unsigned int right_size, unsigned int right_stride, unsigned int answer_size) {
+__global__ void MatMul(float* A, float* B, float* C, unsigned int M, unsigned int N, unsigned int K) {
 
-	unsigned int block_num = my_ceil((float)right_stride / 32);
-	unsigned int block_i = blockIdx.x / block_num;
-	unsigned int block_j = blockIdx.x % block_num;
+	const unsigned int bx = blockIdx.x;
+	const unsigned int by = blockIdx.y;
 
-	unsigned int thread_i = threadIdx.x >> 5;
-	unsigned int thread_j = threadIdx.x % 32u;
+	const unsigned int tx_gmem2smem = threadIdx.x >> 3;
+	const unsigned int ty_gmem2smem = threadIdx.x % 8u;
+	unsigned int offset_gmem2smem[4];
+	offset_gmem2smem[0] = (tx_gmem2smem << 2) * K + ty_gmem2smem;
+	offset_gmem2smem[1] = offset_gmem2smem[0] + K;
+	offset_gmem2smem[2] = offset_gmem2smem[1] + K;
+	offset_gmem2smem[3] = offset_gmem2smem[2] + K;
 
-	float Cij = 0;
-	for (int m = 0; m < my_ceil((float)left_stride / 32); m++){
+	const unsigned int tx_smem2reg = threadIdx.x >> 4;
+	const unsigned int ty_smem2reg = threadIdx.x % 16u;
 
-		__shared__ float left_block[1024];
-		__shared__ float right_block[1024];
+	float* A_begin = A + (bx << 7) * K;
+	float* B_begin = B + (by << 7);
+	float* A_end = A_begin + K;
+	float* C_begin = C + (bx << 7) + (by << 7);
 
-		if (thread_i+(block_i<<5) < left_size / left_stride && thread_j+(m<<5) < left_stride) {
-			left_block[threadIdx.x] = left[((block_i << 5) + thread_i) * left_stride + (m << 5) + thread_j];
-		}
-		else {
-			left_block[threadIdx.x] = 0;
-		}
-		if (thread_i+(m<<5) < right_size / right_stride && thread_j+(block_j<<5) < right_stride) {
-			right_block[threadIdx.x] = right[((m << 5) + thread_i) * right_stride + (block_j << 5) + thread_j];
-		}
-		else {
-			right_block[threadIdx.x] = 0;
-		}
+	float4 A_reg[2] = { {make_float4(0.f, 0.f, 0.f, 0.f)} };
+	float4 B_reg[2] = { {make_float4(0.f, 0.f, 0.f, 0.f)} };
+	float4 panel_1[4] = { {make_float4(0.f, 0.f, 0.f, 0.f)} };
+	float4 panel_2[4] = { {make_float4(0.f, 0.f, 0.f, 0.f)} };
+	float4 panel_3[4] = { {make_float4(0.f, 0.f, 0.f, 0.f)} };
+	float4 panel_4[4] = { {make_float4(0.f, 0.f, 0.f, 0.f)} };
+
+	for (float* A_ptr = A_begin, *B_ptr = B_begin; A_ptr != A_end; A_ptr += 8, B_ptr += N << 3) {
+		__shared__ float4 Asmem[8][32];
+		__shared__ float4 Bsmem[8][32];
+
+		Asmem[ty_gmem2smem][tx_gmem2smem].x = *(A_ptr + offset_gmem2smem[0]);
+		Asmem[ty_gmem2smem][tx_gmem2smem].y = *(A_ptr + offset_gmem2smem[1]);
+		Asmem[ty_gmem2smem][tx_gmem2smem].z = *(A_ptr + offset_gmem2smem[2]);
+		Asmem[ty_gmem2smem][tx_gmem2smem].w = *(A_ptr + offset_gmem2smem[3]);
+
+		Bsmem[ty_gmem2smem][tx_gmem2smem] = *((float4*)(B_ptr + ty_gmem2smem * N + (tx_gmem2smem << 2)));
+
 		__syncthreads();
-
 #pragma unroll
-		for (int k = 0; k < 32; k++)
-		{
-			Cij += left_block[(thread_i << 5) + k] * right_block[(k << 5) + thread_j];
+		for (int i = 0; i < 8; i++) {
+			A_reg[0] = *(&Asmem[i][0] + tx_smem2reg);
+			A_reg[1] = *(&Asmem[i][0] + tx_smem2reg + 16);
+			B_reg[0] = *(&Bsmem[i][0] + ty_smem2reg);
+			B_reg[1] = *(&Bsmem[i][0] + ty_smem2reg + 16);
+
+			panel_1[0].x += A_reg[0].x * B_reg[0].x;
+			panel_1[0].y += A_reg[0].x * B_reg[0].y;
+			panel_1[0].z += A_reg[0].x * B_reg[0].z;
+			panel_1[0].w += A_reg[0].x * B_reg[0].w;
+			panel_1[1].x += A_reg[0].y * B_reg[0].x;
+			panel_1[1].y += A_reg[0].y * B_reg[0].y;
+			panel_1[1].z += A_reg[0].y * B_reg[0].z;
+			panel_1[1].w += A_reg[0].y * B_reg[0].w;
+			panel_1[2].x += A_reg[0].z * B_reg[0].x;
+			panel_1[2].y += A_reg[0].z * B_reg[0].y;
+			panel_1[2].z += A_reg[0].z * B_reg[0].z;
+			panel_1[2].w += A_reg[0].z * B_reg[0].w;
+			panel_1[3].x += A_reg[0].w * B_reg[0].x;
+			panel_1[3].y += A_reg[0].w * B_reg[0].y;
+			panel_1[3].z += A_reg[0].w * B_reg[0].z;
+			panel_1[3].w += A_reg[0].w * B_reg[0].w;
+
+			panel_2[0].x += A_reg[0].x * B_reg[1].x;
+			panel_2[0].y += A_reg[0].x * B_reg[1].y;
+			panel_2[0].z += A_reg[0].x * B_reg[1].z;
+			panel_2[0].w += A_reg[0].x * B_reg[1].w;
+			panel_2[1].x += A_reg[0].y * B_reg[1].x;
+			panel_2[1].y += A_reg[0].y * B_reg[1].y;
+			panel_2[1].z += A_reg[0].y * B_reg[1].z;
+			panel_2[1].w += A_reg[0].y * B_reg[1].w;
+			panel_2[2].x += A_reg[0].z * B_reg[1].x;
+			panel_2[2].y += A_reg[0].z * B_reg[1].y;
+			panel_2[2].z += A_reg[0].z * B_reg[1].z;
+			panel_2[2].w += A_reg[0].z * B_reg[1].w;
+			panel_2[3].x += A_reg[0].w * B_reg[1].x;
+			panel_2[3].y += A_reg[0].w * B_reg[1].y;
+			panel_2[3].z += A_reg[0].w * B_reg[1].z;
+			panel_2[3].w += A_reg[0].w * B_reg[1].w;
+
+			panel_3[0].x += A_reg[1].x * B_reg[0].x;
+			panel_3[0].y += A_reg[1].x * B_reg[0].y;
+			panel_3[0].z += A_reg[1].x * B_reg[0].z;
+			panel_3[0].w += A_reg[1].x * B_reg[0].w;
+			panel_3[1].x += A_reg[1].y * B_reg[0].x;
+			panel_3[1].y += A_reg[1].y * B_reg[0].y;
+			panel_3[1].z += A_reg[1].y * B_reg[0].z;
+			panel_3[1].w += A_reg[1].y * B_reg[0].w;
+			panel_3[2].x += A_reg[1].z * B_reg[0].x;
+			panel_3[2].y += A_reg[1].z * B_reg[0].y;
+			panel_3[2].z += A_reg[1].z * B_reg[0].z;
+			panel_3[2].w += A_reg[1].z * B_reg[0].w;
+			panel_3[3].x += A_reg[1].w * B_reg[0].x;
+			panel_3[3].y += A_reg[1].w * B_reg[0].y;
+			panel_3[3].z += A_reg[1].w * B_reg[0].z;
+			panel_3[3].w += A_reg[1].w * B_reg[0].w;
+
+			panel_4[0].x += A_reg[1].x * B_reg[1].x;
+			panel_4[0].y += A_reg[1].x * B_reg[1].y;
+			panel_4[0].z += A_reg[1].x * B_reg[1].z;
+			panel_4[0].w += A_reg[1].x * B_reg[1].w;
+			panel_4[1].x += A_reg[1].y * B_reg[1].x;
+			panel_4[1].y += A_reg[1].y * B_reg[1].y;
+			panel_4[1].z += A_reg[1].y * B_reg[1].z;
+			panel_4[1].w += A_reg[1].y * B_reg[1].w;
+			panel_4[2].x += A_reg[1].z * B_reg[1].x;
+			panel_4[2].y += A_reg[1].z * B_reg[1].y;
+			panel_4[2].z += A_reg[1].z * B_reg[1].z;
+			panel_4[2].w += A_reg[1].z * B_reg[1].w;
+			panel_4[3].x += A_reg[1].w * B_reg[1].x;
+			panel_4[3].y += A_reg[1].w * B_reg[1].y;
+			panel_4[3].z += A_reg[1].w * B_reg[1].z;
+			panel_4[3].w += A_reg[1].w * B_reg[1].w;
+
 		}
-		__syncthreads();
 	}
-	if(thread_i+(block_i<<5) < answer_size / right_stride && thread_j+(block_j<<5) < right_stride)
-		answer[((block_i << 5) + thread_i) * right_stride + (block_j << 5) + thread_j] = Cij;
+	__syncthreads();
+	float* C_begin1 = C_begin + (tx_smem2reg << 2) * N + (ty_smem2reg << 2);
+	float* C_begin2 = C_begin1 + 64;
+	float* C_begin3 = C_begin + ((tx_smem2reg << 2) + 64) * N + (ty_smem2reg << 2);
+	float* C_begin4 = C_begin3 + 64;
+
+	*C_begin1 = panel_1[0].x;
+	*(C_begin1 + 1) = panel_1[0].y;
+	*(C_begin1 + 2) = panel_1[0].z;
+	*(C_begin1 + 3) = panel_1[0].w;
+	*(C_begin1 + N) = panel_1[1].x;
+	*(C_begin1 + N + 1) = panel_1[1].y;
+	*(C_begin1 + N + 2) = panel_1[1].z;
+	*(C_begin1 + N + 3) = panel_1[1].w;
+	*(C_begin1 + 2 * N) = panel_1[2].x;
+	*(C_begin1 + 2 * N + 1) = panel_1[2].y;
+	*(C_begin1 + 2 * N + 2) = panel_1[2].z;
+	*(C_begin1 + 2 * N + 3) = panel_1[2].w;
+	*(C_begin1 + 3 * N) = panel_1[3].x;
+	*(C_begin1 + 3 * N + 1) = panel_1[3].y;
+	*(C_begin1 + 3 * N + 2) = panel_1[3].z;
+	*(C_begin1 + 3 * N + 3) = panel_1[3].w;
+
+
+	*C_begin2 = panel_2[0].x;
+	*(C_begin2 + 1) = panel_2[0].y;
+	*(C_begin2 + 2) = panel_2[0].z;
+	*(C_begin2 + 3) = panel_2[0].w;
+	*(C_begin2 + N) = panel_2[1].x;
+	*(C_begin2 + N + 1) = panel_2[1].y;
+	*(C_begin2 + N + 2) = panel_2[1].z;
+	*(C_begin2 + N + 3) = panel_2[1].w;
+	*(C_begin2 + 2 * N) = panel_2[2].x;
+	*(C_begin2 + 2 * N + 1) = panel_2[2].y;
+	*(C_begin2 + 2 * N + 2) = panel_2[2].z;
+	*(C_begin2 + 2 * N + 3) = panel_2[2].w;
+	*(C_begin2 + 3 * N) = panel_2[3].x;
+	*(C_begin2 + 3 * N + 1) = panel_2[3].y;
+	*(C_begin2 + 3 * N + 2) = panel_2[3].z;
+	*(C_begin2 + 3 * N + 3) = panel_2[3].w;
+
+	*C_begin3 = panel_3[0].x;
+	*(C_begin3 + 1) = panel_3[0].y;
+	*(C_begin3 + 2) = panel_3[0].z;
+	*(C_begin3 + 3) = panel_3[0].w;
+	*(C_begin3 + N) = panel_3[1].x;
+	*(C_begin3 + N + 1) = panel_3[1].y;
+	*(C_begin3 + N + 2) = panel_3[1].z;
+	*(C_begin3 + N + 3) = panel_3[1].w;
+	*(C_begin3 + 2 * N) = panel_3[2].x;
+	*(C_begin3 + 2 * N + 1) = panel_3[2].y;
+	*(C_begin3 + 2 * N + 2) = panel_3[2].z;
+	*(C_begin3 + 2 * N + 3) = panel_3[2].w;
+	*(C_begin3 + 3 * N) = panel_3[3].x;
+	*(C_begin3 + 3 * N + 1) = panel_3[3].y;
+	*(C_begin3 + 3 * N + 2) = panel_3[3].z;
+	*(C_begin3 + 3 * N + 3) = panel_3[3].w;
+
+	*C_begin4 = panel_4[0].x;
+	*(C_begin4 + 1) = panel_4[0].y;
+	*(C_begin4 + 2) = panel_4[0].z;
+	*(C_begin4 + 3) = panel_4[0].w;
+	*(C_begin4 + N) = panel_4[1].x;
+	*(C_begin4 + N + 1) = panel_4[1].y;
+	*(C_begin4 + N + 2) = panel_4[1].z;
+	*(C_begin4 + N + 3) = panel_4[1].w;
+	*(C_begin4 + 2 * N) = panel_4[2].x;
+	*(C_begin4 + 2 * N + 1) = panel_4[2].y;
+	*(C_begin4 + 2 * N + 2) = panel_4[2].z;
+	*(C_begin4 + 2 * N + 3) = panel_4[2].w;
+	*(C_begin4 + 3 * N) = panel_4[3].x;
+	*(C_begin4 + 3 * N + 1) = panel_4[3].y;
+	*(C_begin4 + 3 * N + 2) = panel_4[3].z;
+	*(C_begin4 + 3 * N + 3) = panel_4[3].w;
+
 }
 
-int main(){
+int main() {
 
-	cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop, 0);
+	//cudaDeviceProp prop;
+	//cudaGetDeviceProperties(&prop, 0);
+	//cout << prop.sharedMemPerBlock << endl;
 
 	CPU = false;
 	GPU = true;
 
 	// Create matrices
-	MatrixFDD left = Eigen::MatrixXf::Random(Left_Row, Left_Col);
-	MatrixFDD right = Eigen::MatrixXf::Random(Right_Row, Right_Col);
-	MatrixFDD answer_cpu = Eigen::MatrixXf::Zero(Left_Row, Right_Col);
-	MatrixFDD answer_gpu = Eigen::MatrixXf::Zero(Left_Row, Right_Col);
+	MatrixFDD_Row left = Eigen::MatrixXf::Random(Left_Row, Left_Col);
+	MatrixFDD_Row right = Eigen::MatrixXf::Random(Right_Row, Right_Col);
+	MatrixFDD_Row answer_cpu = Eigen::MatrixXf::Zero(Left_Row, Right_Col);
+	MatrixFDD_Row answer_gpu = Eigen::MatrixXf::Zero(Left_Row, Right_Col);
 	cout << "The matrices have been created!" << endl;
 
 	// CPU
 	if (CPU) {
-		clock_t start_CPU = clock();
+		DWORD start_CPU = GetTickCount();
 
 		for (int i = 0; i < Left_Row; i++) {
 			for (int j = 0; j < Right_Col; j++) {
@@ -90,13 +243,16 @@ int main(){
 			}
 		}
 
-		clock_t end_CPU = clock();
-		cout <<"CPU time: " << end_CPU - start_CPU << endl;
+		DWORD end_CPU = GetTickCount();
+		cout << "CPU time: " << end_CPU - start_CPU << endl;
 	}
 
 	// GPU
 	if (GPU) {
-		clock_t start_GPU = clock();
+
+		cudaEvent_t start, stop;
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
 
 		float* left_device;
 		float* right_device;
@@ -112,17 +268,21 @@ int main(){
 		float* answer_ptr = answer_gpu.data();
 		cudaMemcpy(answer_device, answer_ptr, sizeof(float) * Left_Row * Right_Col, cudaMemcpyHostToDevice);
 
-		int gridsize = ceil((float)Left_Row / 32) * ceil((float)Right_Col / 32);
-		MatMul << <gridsize, 1024 >> > (left_device, right_device, answer_device, Left_Row * Left_Col, Left_Col, Right_Row * Right_Col, Right_Col,Left_Row*Right_Col);
+		dim3 gridsize((Left_Row + 127) / 128, (Right_Col + 127) / 128);
+
+		cudaEventRecord(start);
+		MatMul << <gridsize, 256 >> > (left_device, right_device, answer_device, Left_Row, Right_Col, Left_Col);
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
 		for (int i = 0; i < Left_Row; i++)
 		{
 			cudaMemcpy(answer_gpu.data() + i * Right_Col, answer_device + i * Right_Col, sizeof(float) * Right_Col, cudaMemcpyDeviceToHost);
 		}
 
-		clock_t end_GPU = clock();
-		cout << "GPU time: " << end_GPU - start_GPU << endl;
+		float time;
+		cudaEventElapsedTime(&time, start, stop);
+		cout << "gpu time: " << time << " ms" << endl;
 	}
-	cout << answer_gpu.isApprox(left * right) << endl;
 
 	return 0;
 }
